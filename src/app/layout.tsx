72687207aea1f0704a0a7ae2aeb37377458880c6
Galal Elsayed <agalal3516@gmail.com>
2025-11-16 16:03:23 +0200

    Add MetadataBase in layout for OG

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,12 @@
 import './globals.css';
+import type {Metadata} from 'next';
 
 type Props = {
   children: React.ReactNode;
+};
+
+export const metadata: Metadata = {
+  metadataBase: new URL('https://crai-ksa.netlify.app'),
 };
 
 export default function RootLayout({children}: Props) {
